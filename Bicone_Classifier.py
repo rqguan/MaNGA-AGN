c28fc6a4a07ec7685617a6a9351bc0f00ae8cd8e
Runquan Guan <39195158+rqguan@users.noreply.github.com>
2022-06-26 21:42:13 -0400

    Add files via upload

--- Bicone_Classifier.py
+++ Bicone_Classifier.py
@@ -5,6 +5,7 @@
 
 
 from cgi import test
+from curses import termattrs
 from tkinter import EW
 import numpy as np
 import cv2
@@ -39,7 +40,7 @@ def normalize(arr, t_min = 0, t_max = 1):
     return norm_arr
 
 
-def generate_profile_histogram(plateifu, method = 'max', smooth = 10, cycle = 2):
+def polar_EW(plateifu, ring = False, method = None , smooth = 10, cycle = 2):
     
     # Input: plateifu
     # method: output array begin with the max or min value in the array
@@ -57,8 +58,21 @@ def generate_profile_histogram(plateifu, method = 'max', smooth = 10, cycle = 2)
     # Set center of image(just in case)
     w = oiii_ew.shape[0]
     h = oiii_ew.shape[1]
+    
+    if ring == False:
+        r1 = 0
+        r2 = round(w/16)
+    elif ring == True:
+        r1 = round(w/2)
+        r2 = round(3*w/4)
+    else:
+        r1 = 0
+        r2 = round(w/2)
 
-    polarImage, ptSettings = polarTransform.convertToPolarImage(oiii_ew, center=[round(w/2), round(h/2)])
+    polarImage, ptSettings = polarTransform.convertToPolarImage(oiii_ew, 
+                                                                initialRadius=r1, finalRadius=r2, 
+                                                                center=[round(w/2), round(h/2)])
+    
     # Integrate the column
     EW_COL = [sum(x)/polarImage.T.shape[0] for x in zip(*polarImage.T)]
 
@@ -91,7 +105,126 @@ def generate_profile_histogram(plateifu, method = 'max', smooth = 10, cycle = 2)
         end_index = [i for i, n in enumerate(EW_SMO_TW) if n == min_index][cycle] # find index of 2nd min
         trunc_EW = np.array(EW_SMO_TW[start_index:end_index]) # truncate from 1st to 2nd min
     else:
+        trunc_EW = EW_SMO_TW
+    
+    # Correct the 0 data error first:
+    if len(trunc_EW) <= 4:
+        trunc_EW = np.linspace(0,200,200)
+    else:
         pass
+    # Normalization 
+    norm_EW = normalize(trunc_EW)
+    
+    # Make them all to the same length through interpolation
+    x = np.linspace(0,len(norm_EW),len(norm_EW))
+    y = norm_EW
+    x2 = np.linspace(0,len(norm_EW),500)
+    f_linear = scipy.interpolate.interp1d(x, y)
+    intp_EW = f_linear(x2)
+        
+    
+    return intp_EW
+
+
+def fourier_classifier(EW_curve, peak_n = 5):
+    
+    # 1. Set curve osillate around  y=0
+    # 2. Take the FT result  from 1~50 because FT saturate at 0. 
+    # Fourier Transform:
+
+    yf = np.abs(fft(EW_curve)[1:30])
+    peak_value = max(yf)
+    peak_index = list(yf).index(peak_value)
+    
+    n_index = peak_n * -1
+
+    yf.sort()
+    loss = sum(np.diff(yf[n_index:]))
+  
+    
+    return peak_index, loss
+
+
+
+def ellip_ring_ew(plateifu, in_r = 0.9, out_r = 1.1, method = None , sig = 5, smooth = 1, cycle = 1):
+
+    maps = Maps(plateifu, bintype='SPX', template='MILESHC-MASTARSSP')
+    oiii_ew = maps.emline_gew_oiii_5008.value
+
+    # Ellipticity Properties
+    r = maps.spx_ellcoo_elliptical_radius.value.round(decimals=2)
+    phi = maps.spx_ellcoo_elliptical_azimuth.value.round(decimals=2)
+    r_re = maps.spx_ellcoo_r_re.value
+    
+    # We select the ring of r = 0.8 er ~ 1.2 er for EW
+    # Make a new array of (EW, phi_er)
+    ew_r_comb = np.array((oiii_ew, r_re)) 
+    #Tanspose the array to make each element in form of (EW, phi_er)
+
+    # Make out of range data = 0, then output the EW only
+    for i in  ew_r_comb.T :
+        for j in i:
+            if j[1] < in_r or j[1] > out_r:
+                j[0] = 0
+            else:
+                pass
+
+    # Output: zeros except for Ring of EW
+    # Actually we only cares about the EW distribution along the phi direction
+    # So we 
+    # 1. make a tuple in the format of (phi, EW).
+    # 2. delete 0 EW elements.
+    # 3. Interpolate the phi vs. EW to 500
+    # 4. make the curve EW curve plot
+
+
+    phi_EW = np.array((phi, ew_r_comb[0]))
+
+    EW_stacked = []
+    for i in phi_EW.T:
+        for j in i:
+            if j[1] != 0:
+                EW_stacked.append(j)
+            else:
+                pass
+        
+    EW_curve = np.array(EW_stacked)
+
+    # Sort the EW along phi direction
+    EW_sort = EW_curve[EW_curve[:, 0].argsort()]
+
+    EW_COL = EW_sort.T[1]
+    
+    # Exclude outlier using 3-sigma variant
+    mean = np.mean(EW_COL)
+    sd = np.std(EW_COL)
+    EW_CLEAN = [x for x in EW_COL if (x >= mean - sig * sd)]
+    EW_CLEAN = [x for x in EW_CLEAN if (x <= mean + sig * sd)]
+    # Smoothening the curve using Gaussian filter
+    EW_SMO = scipy.ndimage.gaussian_filter(EW_CLEAN, sigma = smooth)
+
+    # To better identify the feature, plot two cycles of the galaxy
+    EW_SMO_TW = []
+    
+    for i in range(0, cycle):
+        EW_SMO_TW = EW_SMO_TW + list(EW_SMO)
+
+    # Plot certain cycles
+
+    if method == 'max':
+        # limited the array to N cycles: but from max to max
+        max_index = max(EW_SMO_TW, default = 0) # find max value
+        start_index = [i for i, n in enumerate(EW_SMO_TW) if n == max_index][0] # find index of 1st max
+        end_index = [i for i, n in enumerate(EW_SMO_TW) if n == max_index][cycle] # find index of 2nd max
+        trunc_EW = np.array(EW_SMO_TW[start_index:end_index]) # truncate from 1st to 2nd max
+    elif method == 'min':
+        # limited the array to N cycles: but from max to max
+        min_index = min(EW_SMO_TW, default = 0) # find min value
+        start_index = [i for i, n in enumerate(EW_SMO_TW) if n == min_index][0] # find index of 1st min
+        end_index = [i for i, n in enumerate(EW_SMO_TW) if n == min_index][cycle] # find index of 2nd min
+        trunc_EW = np.array(EW_SMO_TW[start_index:end_index]) # truncate from 1st to 2nd min
+    else:
+        trunc_EW = EW_SMO_TW
     
     # Correct the 0 data error first:
     if len(trunc_EW) <= 4:
@@ -110,24 +243,3 @@ def generate_profile_histogram(plateifu, method = 'max', smooth = 10, cycle = 2)
         
     
     return intp_EW, plateifu
-
-
-def fourier_classifier(sample):
-    
-    # Importing data, use 2 cycles(default)
-    test_EW_hist = sample[0]
-    test_plateifu = sample[1]
-
-    # 1. Set curve osillate around  y=0
-    # 2. Take the FT result  from 1~50 because FT saturate at 0. 
-    # Fourier Transform:
-    yf = np.abs(fft(test_EW_hist-0.5)[1:50])
-    yf.sort()
-    loss = sum(np.diff(yf[-4:]))
-  
-    
-    return loss, test_plateifu
-    
-    
-    
-    
